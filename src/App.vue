ecf7a47bcadd76c2942deab99fb2b37d5df843ff
Rodolfo Roman <rromanv@gmail.com>
2022-08-26 17:29:14 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import { ref } from 'vue'
+  const title = ref('Hello There ✋')
+</script>
+
+<template>
+  <main class="flex justify-center min-h-screen items-center">
+    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
+  </main>
+</template>
