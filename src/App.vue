198c833bec68b3beb4a51e7288f76c6a437dd5de
Rodolfo Roman <rromanv@gmail.com>
2022-08-28 01:08:04 -0500

    fix(lint): added correct tailwind format order

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <main class="flex justify-center min-h-screen items-center">
+  <main class="flex min-h-screen items-center justify-center">
     <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
   </main>
 </template>
